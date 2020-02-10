02eb3fc9746218cff51826d09317953a74b29446
Harriet Bicknell <harrietindubai@gmail.com>
2020-02-10 17:04:32 +0000

    parking zone

--- app/models/space.rb
+++ app/models/space.rb
@@ -3,4 +3,14 @@ class Space < ApplicationRecord
     has_many :tickets
     has_many :car_users, through: :tickets
 
+    def active_space
+        total = 0
+        arr = Space.where(street_name: self.street_name)
+        arr.map do |space|
+        arr2 = space.tickets
+        total += arr2.select { |ticket| ticket.active == true }.count
+        end
+        arr.count - total
+    end
+   
 end
